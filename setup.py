155d9e77ff8b466ce33710c57356014dac0ef448
Emil Johansson <emil.sweden@gmail.com>
2021-09-04 17:50:35 +0200

    Remove deprecated pytest-runner
    https://pypi.org/project/pytest-runner/

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@
 from setuptools import setup
 
 setup(name='mpwalg',
-      version='0.2.1',
+      version='0.2.2',
       description='Calculate a site\'s password',
       author='Emil Johansson',
       author_email='emil.sweden@gmail.com',
       url='https://github.com/emiljoha/pympw',
       py_modules=['mpw'],
-      setup_requires=["pytest-runner"],
       install_requires=['scrypt', 'wheel'],
       tests_require=['pytest', 'pexpect']
 )
