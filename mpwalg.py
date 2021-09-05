e4eedd366e90b8493da0e5eebee48fd44389e64f
Emil Johansson <emil.sweden@gmail.com>
2021-09-05 10:43:26 +0200

    add mpwalg for backwards compatability.

---
+++ mpwalg.py
@@ -0,0 +1,3 @@
+# for backwards compatability with 0.1. Not sure how but the 0.1
+# package on pypi exposes mpwalg
+from mpw import *
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup
 
 setup(name='mpwalg',
-      version='0.2.2',
+      version='0.2.3',
       description='Calculate a site\'s password',
       author='Emil Johansson',
       author_email='emil.sweden@gmail.com',
       url='https://github.com/emiljoha/pympw',
-      py_modules=['mpw'],
+      py_modules=['mpw', 'mpwalg'],
       install_requires=['scrypt', 'wheel'],
       tests_require=['pytest', 'pexpect']
 )
